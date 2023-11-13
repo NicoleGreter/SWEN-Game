dcd2f1ed51c9cccebb4c5ba2cba672b3a3bfd4c7
Nicole Greter <nicole.greter@stud.hslu.ch>
2023-11-13 10:33:57 +0100

    add Game Loop

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-"""
-This is the suggested main entry Python source file for your project
-"""
+import pygame
+
+BLACK = (0, 0, 0)
+
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Definitely not Donkey Kong")
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill(BLACK)
+
+        pygame.display.update()
 
 
 if __name__ == "__main__":
-    print("This is our Software Project")
+    main()
