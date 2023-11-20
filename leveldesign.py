7358c25984d4d1c7bd992abcc037ee2b0ffc790d
Nicole Greter <nicole.greter@stud.hslu.ch>
2023-11-20 17:52:03 +0100

    add File leveldesign

---
+++ leveldesign.py
@@ -0,0 +1,15 @@
+import pygame
+
+num_columns = 60
+num_rows = 50
+tile_size = 16
+
+tilemap_image = pygame.image.load("./images/Leveldesign.png").convert
+
+tilemap_data = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
