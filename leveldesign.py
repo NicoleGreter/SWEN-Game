8b10655534312b75a6ad26ffc8876f25ee6052bd
Nicole Greter <nicole.greter@stud.hslu.ch>
2023-11-20 20:25:16 +0100

    add Klasse Background

--- leveldesign.py
+++ leveldesign.py
@@ -4,7 +4,7 @@ num_columns = 60
 num_rows = 50
 tile_size = 16
 
-tilemap_image = pygame.image.load("./images/Leveldesign.png").convert
+tilemap_image = pygame.image.load("./images/Leveldesign.png")
 
 tilemap_data = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -13,3 +13,14 @@ tilemap_data = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
+
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)  # call Sprite initializer
+        self.image = image_file
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+
+
+BackGround = Background(tilemap_image, [0, 0])
