242549d8776d4ef58391a3edea9974576a69ce9d
Yannick Franzen <yannick.franzen94@gmail.com>
2023-11-27 13:49:16 +0100

    Bewegung mit Kollision kombiniert

--- character.py
+++ character.py
@@ -9,9 +9,11 @@ class Character:
         self.character_height = character_height
         self.game = game
         self.surface = game.screen
-        self.rect = pygame.Rect(
-            self.x, self.y, self.character_width, self.character_height
-        )
+        # self.rect = pygame.Rect(
+        #     self.x, self.y, self.character_width, self.character_height
+        # )
+        self.rect = pygame.Rect(self.x + 20, self.y + 20, 33.3, 43.25)
+
         # Idle blinking_right
         self.idle_images = []
         for i in range(18):
@@ -62,7 +64,7 @@ class Character:
         #     self.x, self.y, self.character_width, self.character_height
         # )
         self.draw()
-        self.movement(20, frame)
+        self.movement(50, frame)
 
     def movement(self, speed, frame):
         keys = pygame.key.get_pressed()
@@ -98,6 +100,11 @@ class Character:
             )
 
     def draw(self):
-        self.rect.x = self.x
-        self.rect.y = self.y
-        self.rect = pygame.draw.rect(self.surface, (0, 0, 0), (self.x, self.y, 75, 75))
+        # Skalierung 75/900 * 400 width = 33.3
+        # Skalierung 75/900 * 519 hight = 43.25
+        self.rect.x = self.x + 18
+        self.rect.y = self.y + 20
+
+    # self.rect = pygame.draw.rect(
+    #     self.surface, (0, 0, 0), (self.x + 20, self.y + 20, 33.3, 43.25)
+    # )
