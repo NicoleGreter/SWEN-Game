bd21a745d45f18065288654c2868f5ccdfd4e960
Yannick Franzen <yannick.franzen94@gmail.com>
2023-12-04 08:27:32 +0100

    Bewegung und blitting entkoppelt, Idling links eingefügt

--- character.py
+++ character.py
@@ -7,6 +7,9 @@ class Character:
         self.y = y
         self.character_width = character_width
         self.character_height = character_height
+        self.left = False
+        self.right = True
+        self.isjump = False
         self.game = game
         self.surface = game.screen
         # Skalierung damit rectangle hinter charackter width = 75/900 * 375 = 31.25 und hight 75/900 * 505 = 42.1
@@ -77,50 +80,64 @@ class Character:
         self.draw()
         self.movement(200, frame)
 
-    def movement(self, speed, frame):
-        isjump = False
-        jumpcount = 10
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_RIGHT] and not (keys[pygame.K_LEFT]):
-            self.surface.blit(
-                self.walking_right_images[frame % len(self.walking_right_images)],
-                (self.x, self.y),
-            )
-            self.x += speed * self.game.delta_time
-            if self.x > self.game.screen_width - 75 + 24:
-                self.x = self.game.screen_width - 75 + 24
-        if keys[pygame.K_LEFT] and not (keys[pygame.K_RIGHT]):
+        if self.left and keys[pygame.K_LEFT]:
             self.surface.blit(
                 self.walking_left_images[frame % len(self.walking_left_images)],
                 (self.x, self.y),
             )
+
+        if self.right and keys[pygame.K_RIGHT]:
+            self.surface.blit(
+                self.walking_right_images[frame % len(self.walking_right_images)],
+                (self.x, self.y),
+            )
+
+    def movement(self, speed, frame):
+        jumpcount = 10
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT] and not (keys[pygame.K_LEFT]):
+            self.right = True
+            self.left = False
+            self.x += speed * self.game.delta_time
+            if self.x > self.game.screen_width - 75 + 24:
+                self.x = self.game.screen_width - 75 + 24
+        if keys[pygame.K_LEFT] and not (keys[pygame.K_RIGHT]):
+            self.right = False
+            self.left = True
             self.x -= speed * self.game.delta_time
             if self.x < -24:
                 self.x = -24
-        if not (isjump):
-            if keys[pygame.K_SPACE]:
-                isjump = True
-        else:
-            if jumpcount >= -10:
-                neg = 1
-                self.surface.blit(
-                    self.jump_start_images[frame % len(self.jump_start_images)],
-                    (self.x, self.y),
-                )
-                if jumpcount < 0:
-                    neg = -1
-                y -= ((jumpcount**2) * 0.5 * neg) * self.game.delta_time
-                jumpcount -= 1
-            else:
-                isjump = False
-                jumpcount = 10
-            # self.y -= speed * self.game.delta_time
-            # if self.x < 0:
-            #     self.x = 0
-        if not (True in keys):
+        # if not (isjump):
+        #     if keys[pygame.K_SPACE]:
+        #         isjump = True
+        # else:
+        #     if jumpcount >= -10:
+        #         neg = 1
+        #         self.surface.blit(
+        #             self.jump_start_images[frame % len(self.jump_start_images)],
+        #             (self.x, self.y),
+        #         )
+        #         if jumpcount < 0:
+        #             neg = -1
+        #         y -= ((jumpcount**2) * 0.5 * neg) * self.game.delta_time
+        #         jumpcount -= 1
+        #     else:
+        #         isjump = False
+        #         jumpcount = 10
+        # self.y -= speed * self.game.delta_time
+        # if self.x < 0:
+        #     self.x = 0
+        if self.right and not (True in keys):
             self.surface.blit(
                 self.idle_images_right[frame % len(self.idle_images_right)],
+                (self.x, self.y),
+            )
+        if self.left and not (True in keys):
+            self.surface.blit(
+                self.idle_images_left[frame % len(self.idle_images_left)],
                 (self.x, self.y),
             )
 
