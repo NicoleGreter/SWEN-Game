7ba6b64f6f9d3683f486825ac2278aa0905bcb9a
Ramona Brunner <ramona.brunner@stud.hslu.ch>
2023-12-12 16:16:08 +0100

    Code kommentiert

--- game_object.py
+++ game_object.py
@@ -2,7 +2,7 @@ import pygame
 
 
 # Initiierung der Klasse "Cup", welche im Game als Sammelobjekt verwendet wird.
-# Über das Rectangle, welches die Spielfigur und das Objekt umrahmt wird die Kollision über die colliderect Funktion möglich
+# Mit dem Rectangle (aus pygame.Rect), wird die Spielfigur und das Objekt umrahmt und so die Kollision über die colliderect Funktion möglich
 class Cup:
     def __init__(self, game, x, y, cup_width, cup_height):
         self.x = x
@@ -10,7 +10,9 @@ class Cup:
         self.cup_width = cup_width
         self.cup_height = cup_height
         self.game = game
-        self.rect = pygame.Rect(self.x, self.y, cup_width, cup_height)
+        self.rect = pygame.Rect(
+            self.x, self.y, cup_width, cup_height
+        )  # hier wird das Rechteck von pygame abgerufen und hinter das Objekt "Cup" mit der entsprechenden Breite und Höhe platziert
         self.is_collected = False
         self.picture_coffee_cup = pygame.image.load("./images/Objects/Coffee_Cup.png")
         self.picture_coffee_cup = pygame.transform.scale(
@@ -21,9 +23,12 @@ class Cup:
         self.rect.x = self.x
         self.rect.y = self.y
         # pygame.draw.rect(self.game.screen, "yellow", self.rect)
+        # die obige Zeile haben wir ausgeklammert, damit das Rechteck nicht effektiv gezeichnet wird und hinter dem Objekt ersichtlich wird. Für die Prüfung der Kollision war es aber sehr hilfreich zu zeichnen.
         self.game.screen.blit(self.picture_coffee_cup, (self.x, self.y))
 
     def update(self):
         self.draw()
         if self.rect.colliderect(self.game.character.rect):
             self.is_collected = True
+            # mit dieser if-Bedingung wird sichergestellt, dass die Variable "is_collected" bei einer Kollision auf "True" gesetzt wird und den Ursprungsstatus von "False" überschreibt
+            # Im Main wird diese Variable in der Update Funktion verwendet um "eingesammelte" Tassen (also "is_collected = True") aus der Liste "cups" zu entfernen.
